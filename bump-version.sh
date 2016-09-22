6a308e6238195df6e4a6c63789fae71dccf6baab
Fridolin Pokorny <fpokorny@redhat.com>
2016-09-22 16:37:38 +0200

    Use tags in git describe

--- bump-version.sh
+++ bump-version.sh
@@ -2,7 +2,7 @@
 
 echo "Generating new version identifier..."
 
-VERSION=`git describe 2>/dev/null || git rev-parse --short HEAD`
+VERSION=`git describe --tags 2>/dev/null || git rev-parse --short HEAD`
 echo -e "selinonlib_version = '${VERSION}'\n" > selinonlib/version.py
 
 echo "Selinonlib version is '${VERSION}'"
