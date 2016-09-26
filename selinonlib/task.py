af287f5d0dacd3ac87055be004f43c8796c3683c
Fridolin Pokorny <fpokorny@redhat.com>
2016-09-26 15:07:37 +0200

    Polish task creation

--- selinonlib/task.py
+++ selinonlib/task.py
@@ -32,73 +32,74 @@ class Task(Node):
     """
     A task representation within the system
     """
-    def __init__(self, name, import_path, class_name=None, storage=None, max_retry=None, retry_countdown=None,
-                 time_limit=None, queue=None, output_schema=None, task_class=None):
+    def __init__(self, name, import_path, storage, opts):
         """
         :param name: name of the task
         :param import_path: tasks's import
-        :param class_name: tasks's class name, if None, 'name' is used
         :param storage: storage that should be used
-        :param max_retry: configured maximum retry count
-        :param retry_countdown: countdown in seconds to retry task in case of failure
-        :param time_limit: configured time limit for task run
-        :param output_schema: task result output schema
-        :param queue: task queue
-        :param task_class: class of the task
+        :param opts: additional options for task
         """
-        if not isinstance(import_path, str):
-            raise ValueError("Error in task '%s' definition - import path should be string; got '%s'"
-                             % (name, import_path))
-
-        if class_name is not None and not isinstance(class_name, str):
-            raise ValueError("Error in task '%s' definition - class instance should be string; got '%s'"
-                             % (name, class_name))
-
-        if output_schema is not None and not isinstance(output_schema, str):
-            raise ValueError("Error in task '%s' definition - output schema should be string; got '%s'"
-                             % (name, output_schema))
-
-        if max_retry is not None and (not isinstance(max_retry, int) or max_retry < 0):
-            raise ValueError("Error in task '%s' definition - max_retry should be None, zero or positive integer;"
-                             " got '%s'" % (name, max_retry))
-
-        if time_limit is not None and (not isinstance(time_limit, int) or time_limit <= 0):
-            raise ValueError("Error in task '%s' definition - time_limit should be None or positive integer;"
-                             " got '%s'" % (name, time_limit))
-
-        if retry_countdown is not None and (not isinstance(retry_countdown, int) or retry_countdown < 0):
-            raise ValueError("Error in task '%s' definition - retr_countdown should be None or positive integer;"
-                             " got '%s'" % (name, retry_countdown))
-
-        if queue is not None and not isinstance(queue, str):
-            raise ValueError("Invalid task queue, should be string, got %s" % queue)
-
         super(Task, self).__init__(name)
+
+        self.class_name = opts.get('classname', name)
+        self.output_schema = opts.get('output_schema')
+        self.max_retry = opts.get('max_retry', _DEFAULT_MAX_RETRY)
+        self.time_limit = opts.get('time_limit')
+        self.retry_countdown = opts.get('retry_countdown', _DEFAULT_RETRY_COUNTDOWN)
+        self.queue_name = opts.get('queue', GlobalConfig.default_task_queue)
         self.import_path = import_path
-        self.max_retry = max_retry
-        self.retry_countdown = retry_countdown
-        self.time_limit = time_limit
-        self.class_name = class_name if class_name else name
         self.storage = storage
-        self.output_schema = output_schema
-        self.task_class = task_class
-        self.queue_name = queue or GlobalConfig.default_task_queue
+
         # register task usage
-        if storage:
+        if self.storage:
             storage.register_task(self)
         _logger.debug("Creating task with name '%s' import path '%s', class name '%s'"
                       % (self.name, self.import_path, self.class_name))
 
+    def check(self):
+        """
+        Check task definitions for errors
+
+        :raises: ValueError if an error occurred
+        """
+        if not isinstance(self.import_path, str):
+            raise ValueError("Error in task '%s' definition - import path should be string; got '%s'"
+                             % (self.name, self.import_path))
+
+        if self.class_name is not None and not isinstance(self.class_name, str):
+            raise ValueError("Error in task '%s' definition - class instance should be string; got '%s'"
+                             % (self.name, self.class_name))
+
+        if self.output_schema is not None and not isinstance(self.output_schema, str):
+            raise ValueError("Error in task '%s' definition - output schema should be string; got '%s'"
+                             % (self.name, self.output_schema))
+
+        if self.max_retry is not None and (not isinstance(self.max_retry, int) or self.max_retry < 0):
+            raise ValueError("Error in task '%s' definition - max_retry should be None, zero or positive integer;"
+                             " got '%s'" % (self.name, self.max_retry))
+
+        if self.time_limit is not None and (not isinstance(self.time_limit, int) or self.time_limit <= 0):
+            raise ValueError("Error in task '%s' definition - time_limit should be None or positive integer;"
+                             " got '%s'" % (self.name, self.time_limit))
+
+        if self.retry_countdown is not None and (not isinstance(self.retry_countdown, int) or self.retry_countdown < 0):
+            raise ValueError("Error in task '%s' definition - retry_countdown should be None or positive integer;"
+                             " got '%s'" % (self.name, self.retry_countdown))
+
+        if self.queue_name is not None and not isinstance(self.queue_name, str):
+            raise ValueError("Invalid task queue, should be string, got %s" % self.queue_name)
+
     @staticmethod
     def from_dict(d, system):
         """
-        Construct task from a dict
+        Construct task from a dict and check task's definition correctness
 
         :param d: dictionary to be used to construct the task
         :return: Task instance
         :param system: system that should be used to for lookup a storage
         :type system: System
         :rtype: Task
+        :raises: ValueError
         """
         if 'name' not in d or not d['name']:
             raise KeyError('Task name definition is mandatory')
@@ -109,7 +110,6 @@ class Task(Node):
         else:
             storage = None
 
-        # TODO: this looks pretty ugly, refactor this
-        return Task(d['name'], d['import'], d.get('classname'), storage, d.get('max_retry', _DEFAULT_MAX_RETRY),
-                    d.get('retry_countdown', _DEFAULT_RETRY_COUNTDOWN), d.get('time_limit'), d.get('queue'),
-                    d.get('output_schema'))
+        instance = Task(d['name'], d['import'], storage, d)
+        instance.check()
+        return instance
