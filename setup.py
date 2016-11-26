eedcca741a982ab4c4d0f67af65c47b6ed8576b7
Fridolin Pokorny <fpokorny@redhat.com>
2016-11-26 15:52:18 +0100

    Centralize version information

--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
 
 import sys
+import os
 from setuptools import setup, find_packages
+
+
+NAME = 'selinonlib'
 
 
 def get_requirements():
     with open('requirements.txt') as fd:
         return fd.read().splitlines()
 
+
+def get_version():
+    with open(os.path.join(NAME, 'version.py')) as f:
+        version = f.readline()
+    return version.split(' = ')[1]
+
+
 if sys.version_info[0] != 3:
     sys.exit("Python3 is required in order to install selinonlib")
 
 setup(
-    name='selinonlib',
+    name=NAME,
     version='0.1.0rc3',
     packages=find_packages(),
     scripts=['selinonlib-cli'],
@@ -23,7 +34,7 @@ setup(
     maintainer_email='fpokorny@redhat.com',
     description='a simple tool to visualize, check and generate Python code from a YAML configuration file for Selinon'
                 ' dispatcher for Celery',
-    url='https://github.com/fridex/selinonlib',
+    url='https://github.com/selinon/selinonlib',
     license='GPLv2+',
     keywords='node task graph edge celery selinon yaml condition',
     classifiers=[
