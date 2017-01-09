624264461ce0962196a265722b5a7870eeeb3fc1
Fridolin Pokorny <fpokorny@redhat.com>
2017-01-09 16:19:19 +0100

    State storages in storages package

--- selinonlib/storages/__init__.py
+++ selinonlib/storages/__init__.py
@@ -2,3 +2,7 @@
 """Various pre-implemented database adapters for Selinon for storage usage"""
 
 from .dataStorage import DataStorage
+from .inMemoryStorage import InMemoryStorage
+from .mongoStorage import MongoStorage
+from .redisStorage import RedisStorage
+from .sqlStorage import SqlStorage
