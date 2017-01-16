b9f76545ac403f91a446f1de3b10701b31722c28
Fridolin Pokorny <fpokorny@redhat.com>
2017-01-16 08:14:28 +0100

    Let a user specify connection details for S3 storage

--- selinonlib/storages/s3.py
+++ selinonlib/storages/s3.py
@@ -34,19 +34,26 @@ class S3(DataStorage):
     Amazon S3 storage adapter, for credentials configuration see boto3 library configuration
     https://github.com/boto/boto3
     """
-    def __init__(self, bucket, location):
+    def __init__(self, bucket, location, endpoint_url=None, use_ssl=None,
+                 aws_access_key_id=None, aws_secret_access_key=None, region_name=None):
         # AWS access key and access id are handled by Boto - place them to config or use env variables
         super().__init__()
         self._bucket_name = bucket
         self._location = location
         self._s3 = None
+        self._use_ssl = use_ssl
+        self._endpoint_url = endpoint_url
+        self._session = boto3.session.Session(aws_access_key_id=aws_access_key_id,
+                                              aws_secret_access_key=aws_secret_access_key,
+                                              region_name=region_name)
 
     def is_connected(self):
         return self._s3 is not None
 
     def connect(self):
         # we need signature version v4 as new AWS regions use this version and we won't be able to connect without this
-        self._s3 = boto3.resource('s3', config=botocore.client.Config(signature_version='s3v4'))
+        self._s3 = self._session.resource('s3', config=botocore.client.Config(signature_version='s3v4'),
+                                          use_ssl=self._use_ssl, endpoint_url=self._endpoint_url)
 
         # check that the bucket exists - see boto docs
         try:
